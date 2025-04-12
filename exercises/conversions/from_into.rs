e58c7e1d103dbebd49682291d0c571e3f426c358
lengjing12360 <910346899@qq.com>
2025-04-12 18:29:52 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let person: Vec<&str> = s.split(",").collect();
+        if person.len() > 2 {
+            return Person::default();
+        }
+        let name = person[0];
+        if name == "" {
+            return Person::default();
+        }
+        match person.get(1) {
+            Some(age_str) => {
+                match age_str.parse::<usize>() {
+                    Ok(age) => Person{name: name.to_string(), age: age},
+                    _ => Person::default()
+                }
+            },
+            None => Person::default()
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(Self::Err::Empty);
+        }
+        let person: Vec<_> = s.split(",").collect();
+        if person.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+        let name = person[0];
+        if name.len() == 0 {
+            return Err(Self::Err::NoName);
+        }
+        match person[1].parse::<usize>() {
+            Ok(x) => Ok(Person{name: name.to_string(), age: x}),
+            Err(x) => Err(Self::Err::ParseInt(x))
+        }
     }
 }
 
